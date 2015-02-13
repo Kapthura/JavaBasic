39e27e758c1c135ebb8deb64ff55baa972cf8c30
Kapthura jayasanka <kapthura.public@gmail.com>
2015-02-13 19:29:07 +0530

    Control Flow Statement
    Control Flow Statement !!!!!

--- src/com/kaps/javabasic/JavaBasic.java
+++ src/com/kaps/javabasic/JavaBasic.java
@@ -24,21 +24,28 @@ public class JavaBasic {
          name = co.getName();
 
          co.display(name);*/
-        //EX 02
-        //Constructors
-        //Calling to default Constructor
-        ConstructorClass cc = new ConstructorClass();
-        cc.display();
+        /* //Ex 02
+         //Constructors
+         //Calling to default Constructor
+         ConstructorClass cc = new ConstructorClass();
+         cc.display();
         
-        //Calling to parameterized Constructor 01
-        ConstructorClass cc1= new ConstructorClass(123);
-        cc1.display();
+         //Calling to parameterized Constructor 01
+         ConstructorClass cc1= new ConstructorClass(123);
+         cc1.display();
 
-        //Calling to parameterized Constructor 02
-        ConstructorClass cc2 = new ConstructorClass(456, "Kaps");
-        cc2.display();
-
-        
+         //Calling to parameterized Constructor 02
+         ConstructorClass cc2 = new ConstructorClass(456, "Kaps");
+         cc2.display();*/
+        //Ex 03
+        //Control Flow Statement
+        //Switch Statement 
+        //Send month no display month name
+        ControlFlowStatementClass cfs = new ControlFlowStatementClass();
+        //cfs.switchM(14);
+        // cfs.dowhile();
+        //cfs.forM1();
+         cfs.forM2();
     }
 
 }
